f407b1f54dcb1b32eae2c5da3a8f999d564a2f6e
Kimmy <Kimmy_11_07@hotmail.com>
2016-06-22 21:20:16 -0500

    Nya

--- Module/Module/Module1.vb
+++ Module/Module/Module1.vb
@@ -3,6 +3,7 @@
     Sub Main()
         For i As Integer = 0 To 5 Step 1
             Console.WriteLine("Te amo mucho")
+            Console.WriteLine("Te amo muuuuuuucho mas <3")
         Next
         Console.ReadLine()
     End Sub
