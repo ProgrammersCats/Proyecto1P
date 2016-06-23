f043176b2a6450570639eb52cd7200e6e8aa80e8
PaulValle <paul.valle96@gmail.com>
2016-06-22 21:16:43 -0500

    Module

---
+++ Module/Module/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        For i As Integer = 0 To 5 Step 1
+            Console.WriteLine("Te amo mucho")
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
