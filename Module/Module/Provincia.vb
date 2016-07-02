06957a778308c8aed15ad53e61ed6590c1ce439b
PaulValle <paul.valle96@gmail.com>
2016-07-02 12:48:41 -0500

    Clase Provincia con atrivutos y propiedades

--- Module/Module/Provincia.vb
+++ Module/Module/Provincia.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Provincia
+    Private _nombreProvincia As String
+    Public Property NombreProvincia() As String
+        Get
+            Return _nombreProvincia
+        End Get
+        Set(ByVal value As String)
+            _nombreProvincia = value
+        End Set
+    End Property
 
+    Private _iva As Double
+    Public Property Iva() As Double
+        Get
+            Return _iva
+        End Get
+        Set(ByVal value As Double)
+            _iva = value
+        End Set
+    End Property
 End Class
