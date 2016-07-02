d920391cd1d7a878aadaa574a1178112bf91e2fa
PaulValle <paul.valle96@gmail.com>
2016-07-02 12:26:58 -0500

    Clase Producto con atributos y propiedades

--- Module/Module/Producto.vb
+++ Module/Module/Producto.vb
@@ -1,3 +1,40 @@
 ﻿Public Class Producto
+    Private _nombreProducto As String
+    Public Property NombreProducto As String
+        Get
+            Return _nombreProducto
+        End Get
+        Set(ByVal value As String)
+            _nombreProducto = value
+        End Set
+    End Property
 
+    Private _pvp As Double
+    Public Property Pvp() As Double
+        Get
+            Return _pvp
+        End Get
+        Set(ByVal value As Double)
+            _pvp = value
+        End Set
+    End Property
+
+    Private _precioFabrica As Double
+    Public Property PrecioFabrica() As Double
+        Get
+            Return _precioFabrica
+        End Get
+        Set(ByVal value As Double)
+            _precioFabrica = value
+        End Set
+    End Property
+
+    Public Sub New()
+
+    End Sub
+    Public Sub New(nombreProducto As String, pvp As Double, precioFabrica As Double)
+        Me.NombreProducto = nombreProducto
+        Me.Pvp = pvp
+        Me.PrecioFabrica = precioFabrica
+    End Sub
 End Class
