f8484d9f5ce29e42c1655d21e827568d49516304
PaulValle <paul.valle96@gmail.com>
2016-07-02 14:56:16 -0500

    Clase Repositorio

--- Module/Module/RepositorioProductos.vb
+++ Module/Module/RepositorioProductos.vb
@@ -1,3 +1,23 @@
 ﻿Public Class RepositorioProductos
+    Private _arrayProductos As ArrayList
+    Public Property ArrayProductos() As ArrayList
+        Get
+            Return _arrayProductos
+        End Get
+        Set(ByVal value As ArrayList)
+            _arrayProductos = value
+        End Set
+    End Property
+
+    Public Sub AgregarProducto(producto As Producto)
+        Me._arrayProductos.Add(producto)
+    End Sub
+
+    Public Sub MostrarInventario()
+        Console.WriteLine("********* INVENTARIO DE PRODUCTOS ***********")
+        For Each producto As Producto In Me._arrayProductos
+            producto.ToString()
+        Next
+    End Sub
 
 End Class
