ef5c7d075d6121d00b166a1f493b356521a817f4
PaulValle <paul.valle96@gmail.com>
2016-07-02 09:29:39 -0500

    Clase Admin

---
+++ Module/Module/Admin.vb
@@ -0,0 +1,17 @@
+﻿Public Class Admin
+    Inherits Persona
+    Private _idAdministrador As Integer
+    Public Property IdAdmin() As Integer
+        Get
+            Return _idAdministrador
+        End Get
+        Set(ByVal value As Integer)
+            _idAdministrador = value
+        End Set
+    End Property
+    Public Sub New(nombre As String, apellido As String, idAdministrador As Integer)
+        MyBase.New(nombre, apellido)
+        Me.IdAdmin = idAdministrador
+    End Sub
+
+End Class
