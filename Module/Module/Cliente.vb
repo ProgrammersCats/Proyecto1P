b1dfff0605c75c0bf72f56c31f536b7aac78b494
PaulValle <paul.valle96@gmail.com>
2016-07-02 09:06:11 -0500

    Clase Cliente

---
+++ Module/Module/Cliente.vb
@@ -0,0 +1,16 @@
+﻿Public Class Cliente
+    Inherits Persona
+    Private _idCliente As Integer
+    Public Property IdCliente() As Integer
+        Get
+            Return _idCliente
+        End Get
+        Set(ByVal value As Integer)
+            _idCliente = value
+        End Set
+    End Property
+    Public Sub New(nombre As String, apellido As String, idcliente As Integer)
+        MyBase.New(nombre, apellido)
+        Me.IdCliente = idcliente
+    End Sub
+End Class
